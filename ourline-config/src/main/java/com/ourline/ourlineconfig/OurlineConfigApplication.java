019cd224e913500ba5e69fdda025d880110d5807
peiliang <214190965@qq.com>
2021-01-18 17:33:15 +0800

    ourline-config

---
+++ ourline-config/src/main/java/com/ourline/ourlineconfig/OurlineConfigApplication.java
@@ -0,0 +1,13 @@
+package com.ourline.ourlineconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurlineConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurlineConfigApplication.class, args);
+    }
+
+}
---
+++ ourline-config/src/test/java/com/ourline/ourlineconfig/OurlineConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ourline.ourlineconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurlineConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
