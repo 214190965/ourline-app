30312050620f7975763ffd9ff846a8a2143181c5
peiliang <214190965@qq.com>
2021-01-20 11:10:24 +0800

    zuul

---
+++ ourline-zuul/src/main/java/com/ourline/ourlinezuul/OurlineZuulApplication.java
@@ -0,0 +1,13 @@
+package com.ourline.ourlinezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurlineZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurlineZuulApplication.class, args);
+    }
+
+}
---
+++ ourline-zuul/src/test/java/com/ourline/ourlinezuul/OurlineZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ourline.ourlinezuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurlineZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
